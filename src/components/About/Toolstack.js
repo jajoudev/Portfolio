c8e9832b6ed2068485f2a7c45dba6b32927d5b66
Jajou 猫 <jahelgithub64@gmail.com>
2025-12-18 15:25:45 +0100

    Changed icons

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -3,17 +3,23 @@ import { Col, Row } from "react-bootstrap";
 // import macOs from "../../Assets/TechIcons/Apple MacOSX.svg";
 import chrome from "../../Assets/TechIcons/Google Chrome.svg";
 import vsCode from "../../Assets/TechIcons/vscode.svg";
+import opera from "../../Assets/TechIcons/Opera.svg";
+import github_desktop from "../../Assets/TechIcons/Github-Desktop.svg";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <img src={macOs} alt="macOs" className="tech-icon-images" />
-        <div className="tech-icons-text">Mac Os</div>
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons ">
         <img src={chrome} alt="Chrome" className="tech-icon-images" />
         <div className="tech-icons-text">Google Chrome</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons ">
+        <img src={opera} alt="Opera" className="tech-icon-images" />
+        <div className="tech-icons-text">Opera</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons ">
+        <img src={github_desktop} alt="github_desktop" className="tech-icon-images" />
+        <div className="tech-icons-text">Github Desktop</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons ">
         <img src={vsCode} alt="vsCode" className="tech-icon-images" />
